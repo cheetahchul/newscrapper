d6ad955294a31d59672141ff7ae5fc83404710cd
cheetahchul <cheetahchul@users.noreply.github.com>
2021-04-01 22:24:27 +0900

    day1-1

--- indeed.py
+++ indeed.py
@@ -19,21 +19,25 @@ def extract_indeed_pages():
     return last_page
 
 
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    return {"title": title, "company": company}
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
     for page in range(last_page):
         result = requests.get(f"{URL}&start={page*LIMIT}")
     soup = BeautifulSoup(result.text, "html.parser")
     results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
-
     for result in results:
-        title = result.find("h2", {"class": "title"}).find("a")["title"]
-        company = result.find("span", {"class": "company"})
-        company_anchor = company.find("a")
-        if company_anchor is not None:
-            company = str(company_anchor.string)
-        else:
-            company = str(company.string)
-        company = company.strip()
-        print(company)
+        job = extract_job(result)
+        print(job)
     return jobs
